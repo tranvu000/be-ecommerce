c37dfa65b0c17a0d0634c4f91b17848d26546d6e
Vu Tran <tranvietvu0218@gmail.com>
2024-01-04 11:18:46 +0700

    Design database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config;
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +10,14 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('hello')
 });
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log('Connect DB success!');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 
 app.listen(port, () => {
   console.log('Server is running in port: ', port);
