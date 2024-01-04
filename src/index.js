8a3a4980f3c695d82ec0784d2b0e4b2631f71743
Vu Tran <tranvietvu0218@gmail.com>
2024-01-04 08:46:25 +0700

    Set up for the project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config;
 
 const app = express();
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('hello')
+});
 
 app.listen(port, () => {
   console.log('Server is running in port: ', port);
